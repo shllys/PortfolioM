c9298dc5a436e53375d4810f750d4f8563d1cfab
shellys <shllygp17.8@gmail.com>
2024-01-24 17:15:12 -0400

    add portfolio first

---
+++ README.md
@@ -0,0 +1,14 @@
+# Creacion de "Portfolio First"
+
+He creado de a poco mi primer portafolio con los elementos básicos del desarrollo web:
+
+<div style="display:flex">
+    <img src="./icons/logo-html5.svg" alt="" />
+    <img src="./icons/logo-css3.svg" alt="" />
+    <img src="./icons/logo-javascript.svg" alt="" />
+</div>
+
+Y con una librería:
+    <img src="./icons/logo-sass.svg" alt="" />
+
+Realizando con esfuerzo y aprendiendo otras herramientas para una mejor elaboración.
---
+++ app.js
@@ -0,0 +1,155 @@
+const all = document.querySelector(".principal");
+const card2 = document.querySelector(".about");
+const design = document.querySelector(".design");
+const pages = document.querySelector(".pages");
+const contact = document.querySelector(".contact");
+
+// skill----------------
+function frontS() {
+  all.classList.remove("active");
+  all.classList.add("inactive");
+
+  card2.classList.toggle("inactive");
+  card2.classList.toggle("active");
+
+  design.classList.add("inactive");
+  design.classList.remove("active");
+
+  pages.classList.add("inactive");
+  pages.classList.remove("active");
+
+  contact.classList.add("inactive");
+  contact.classList.remove("active");
+}
+// skill----------------
+
+// design----------------
+function portD() {
+  all.classList.remove("active");
+  all.classList.add("inactive");
+
+  design.classList.toggle("inactive");
+  design.classList.toggle("active");
+
+  card2.classList.add("inactive");
+  card2.classList.remove("active");
+
+  pages.classList.add("inactive");
+  pages.classList.remove("active");
+
+  contact.classList.add("inactive");
+  contact.classList.remove("active");
+}
+// design----------------
+
+// code-------------------
+function portP() {
+  all.classList.remove("active");
+  all.classList.add("inactive");
+
+  design.classList.add("inactive");
+  design.classList.remove("active");
+
+  pages.classList.toggle("inactive");
+  pages.classList.toggle("active");
+
+  card2.classList.add("inactive");
+  card2.classList.remove("active");
+
+  contact.classList.add("inactive");
+  contact.classList.remove("active");
+}
+// code-------------------
+
+// contact-------------------
+function frontC() {
+  all.classList.remove("active");
+  all.classList.add("inactive");
+
+  design.classList.add("inactive");
+  design.classList.remove("active");
+
+  contact.classList.toggle("inactive");
+  contact.classList.toggle("active");
+
+  pages.classList.add("inactive");
+  pages.classList.remove("active");
+
+  card2.classList.add("inactive");
+  card2.classList.remove("active");
+}
+// contact-------------------
+
+// removeBack -------------------
+function back() {
+  card2.classList.remove("active");
+  card2.classList.add("inactive");
+
+  all.classList.toggle("active");
+  all.classList.toggle("inactive");
+
+  design.classList.remove("active");
+  design.classList.add("inactive");
+
+  pages.classList.add("inactive");
+  pages.classList.remove("active");
+
+  contact.classList.add("inactive");
+  contact.classList.remove("active");
+}
+// removeBack -------------------
+
+// CopyPaper ---------------
+function copy() {
+  let url = document.querySelector(".gmail");
+  let msj = document.querySelector(".mensaje");
+
+  navigator.clipboard.writeText(url.textContent);
+  msj.innerHTML = "Gmail Copiado!";
+
+  setTimeout(() => (msj.textContent = ""), 2000);
+}
+// CopyPaper ---------------
+
+// PortMenu ---------------
+const list = document.querySelectorAll(".list");
+
+list.forEach(function (item) {
+  const portMenu = item.querySelector(".port");
+  const check = item.querySelector(".menu");
+
+  check.addEventListener("click", () => {
+    portMenu.classList.toggle("in-menu");
+  });
+});
+// PortMenu ---------------
+
+// PageInfo -------------------
+
+const contenedorLight = document.querySelectorAll('.imagen-light');
+
+contenedorLight.forEach(contain => {
+    let light = contain.querySelector('.agregar-imagen');
+    let imagenes = document.querySelectorAll('.img');
+
+    imagenes.forEach (imagen =>{
+        imagen.addEventListener('click', () => {
+            aparecerImagen(imagen.getAttribute('src'))
+        })
+    })
+
+    contain.addEventListener('click', (e) => {
+        if (e.target !== light){
+            contain.classList.toggle('show')
+            light.classList.toggle('showImagen')
+        }
+    })
+
+    const aparecerImagen = (imagen) =>{
+        light.src = imagen;
+        contain.classList.toggle('show')
+        light.classList.toggle('showImagen')
+    }
+})
+
+// PageInfo -------------------
